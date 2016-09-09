0a663a9f212a490fde8c34510e532dc4a19b8b97
agileabc <agileabc@sina.com>
2016-09-09 14:09:12 +0800

    Init.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yui-test
+
+Open index.html to see the test result. 
---
+++ yui-test.js
@@ -0,0 +1,35 @@
+
+YUI().use('test', function (Y) {
+    // Test is available and ready for use. Add implementation
+    // code here.
+    var testCase = new Y.Test.Case({
+
+        name: "TestCase Name",
+
+        //---------------------------------------------
+        // Setup and tear down
+        //---------------------------------------------
+
+        setUp : function () {
+            this.data = { name : "Nicholas", age : 28 };
+        },
+
+        tearDown : function () {
+            delete this.data;
+        },
+
+        //---------------------------------------------
+        // Tests
+        //---------------------------------------------
+
+        testName: function () {
+            Y.Assert.areEqual("Nicholas", this.data.name, "Name should be 'Nicholas'");
+        },
+
+        testAge: function () {
+            Y.Assert.areEqual(28, this.data.age, "Age should be 28");
+        }
+    });
+
+});
+
